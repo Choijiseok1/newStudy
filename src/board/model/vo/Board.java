9b6d81a159e86a56a6b73bb68f7c2461b8377e3a
user1 <user1@KH_H>
2018-03-20 10:16:32 +0900

    2

--- src/board/model/vo/Board.java
+++ src/board/model/vo/Board.java
@@ -3,148 +3,152 @@ package board.model.vo;
 import java.sql.Date;
 
 public class Board implements java.io.Serializable{
-private static final long serialVersionUID=12l;
+	private static final long serialVersionUID = 12L;
 	
+	private int boardNum;
+	private String boardWriter;
+	private String boardTitle;
+	private String boardContent;
+	private String boardOriginalFileName;
+	private String boardRenameFileName;
+	private int boardRef;	//참조하는 원글번호
+	private int boardReplyRef;  //참조하는 댓글 번호, 원글은 0
+	private int boardReplyLev;  //원글:0, 원글의댓글:1, 댓글의댓글:2
+	private int boardReplySeq;  //댓글의 순번, 최근 댓글이 1
+	private int boardReadCount; //읽은 조회수
+	private Date boardDate;     //작성날짜
 	
+	public Board() {}	
+	
+	public Board(int boardNum, String boardWriter, String boardTitle, String boardContent, String boardOriginalFileName,
+			String boardRenameFileName, int boardRef, int boardReplyRef, int boardReplyLev, int boardReplySeq,
+			int boardReadCount, Date boardDate) {
+		super();
+		this.boardNum = boardNum;
+		this.boardWriter = boardWriter;
+		this.boardTitle = boardTitle;
+		this.boardContent = boardContent;
+		this.boardOriginalFileName = boardOriginalFileName;
+		this.boardRenameFileName = boardRenameFileName;
+		this.boardRef = boardRef;
+		this.boardReplyRef = boardReplyRef;
+		this.boardReplyLev = boardReplyLev;
+		this.boardReplySeq = boardReplySeq;
+		this.boardReadCount = boardReadCount;
+		this.boardDate = boardDate;
+	}	
 
-public Board() {
-		// TODO Auto-generated constructor stub
+	public int getBoardNum() {
+		return boardNum;
 	}
 
-private int boardNum;
-private String boardWriter;
-private String boardTitle;
-private String boardContent;
-private String boardOriginalFileName;
-private String boardRenameFileName;
-private int boardReplyRef;  //참조하는 원글 번호, 원글은 자기번호
-private int boardReplyLev;  //원글:0, 원글의댓글:1, 댓글의댓글:2
-private int boardReplySeq;  //댓글의 순번, 최근 댓글이 1
-private int boardReadCount; //읽은 조회수
-private Date boardDate;     //작성날짜
+	public void setBoardNum(int boardNum) {
+		this.boardNum = boardNum;
+	}
 
+	public String getBoardWriter() {
+		return boardWriter;
+	}
 
-public Board(int boardNum, String boardWriter, String boardTitle, String boardContent, String boardOriginalFileName,
-		String boardRenameFileName, int boardReplyRef, int boardReplyLev, int boardReplySeq, int boardReadCount,
-		Date boardDate) {
-	super();
-	this.boardNum = boardNum;
-	this.boardWriter = boardWriter;
-	this.boardTitle = boardTitle;
-	this.boardContent = boardContent;
-	this.boardOriginalFileName = boardOriginalFileName;
-	this.boardRenameFileName = boardRenameFileName;
-	this.boardReplyRef = boardReplyRef;
-	this.boardReplyLev = boardReplyLev;
-	this.boardReplySeq = boardReplySeq;
-	this.boardReadCount = boardReadCount;
-	this.boardDate = boardDate;
-}
+	public void setBoardWriter(String boardWriter) {
+		this.boardWriter = boardWriter;
+	}
 
-public int getBoardNum() {
-	return boardNum;
-}
+	public String getBoardTitle() {
+		return boardTitle;
+	}
 
-public void setBoardNum(int boardNum) {
-	this.boardNum = boardNum;
-}
+	public void setBoardTitle(String boardTitle) {
+		this.boardTitle = boardTitle;
+	}
 
-public String getBoardWriter() {
-	return boardWriter;
-}
+	public String getBoardContent() {
+		return boardContent;
+	}
 
-public void setBoardWriter(String boardWriter) {
-	this.boardWriter = boardWriter;
-}
+	public void setBoardContent(String boardContent) {
+		this.boardContent = boardContent;
+	}
 
-public String getBoardTitle() {
-	return boardTitle;
-}
+	public String getBoardOriginalFileName() {
+		return boardOriginalFileName;
+	}
 
-public void setBoardTitle(String boardTitle) {
-	this.boardTitle = boardTitle;
-}
+	public void setBoardOriginalFileName(String boardOriginalFileName) {
+		this.boardOriginalFileName = boardOriginalFileName;
+	}
 
-public String getBoardContent() {
-	return boardContent;
-}
+	public String getBoardRenameFileName() {
+		return boardRenameFileName;
+	}
 
-public void setBoardContent(String boardContent) {
-	this.boardContent = boardContent;
-}
+	public void setBoardRenameFileName(String boardRenameFileName) {
+		this.boardRenameFileName = boardRenameFileName;
+	}
 
-public String getBoardOriginalFileName() {
-	return boardOriginalFileName;
-}
+	public int getBoardRef() {
+		return boardRef;
+	}
 
-public void setBoardOriginalFileName(String boardOriginalFileName) {
-	this.boardOriginalFileName = boardOriginalFileName;
-}
+	public void setBoardRef(int boardRef) {
+		this.boardRef = boardRef;
+	}
 
-public String getBoardRenameFileName() {
-	return boardRenameFileName;
-}
+	public int getBoardReplyRef() {
+		return boardReplyRef;
+	}
 
-public void setBoardRenameFileName(String boardRenameFileName) {
-	this.boardRenameFileName = boardRenameFileName;
-}
+	public void setBoardReplyRef(int boardReplyRef) {
+		this.boardReplyRef = boardReplyRef;
+	}
 
-public int getBoardReplyRef() {
-	return boardReplyRef;
-}
+	public int getBoardReplyLev() {
+		return boardReplyLev;
+	}
 
-public void setBoardReplyRef(int boardReplyRef) {
-	this.boardReplyRef = boardReplyRef;
-}
+	public void setBoardReplyLev(int boardReplyLev) {
+		this.boardReplyLev = boardReplyLev;
+	}
 
-public int getBoardReplyLev() {
-	return boardReplyLev;
-}
+	public int getBoardReplySeq() {
+		return boardReplySeq;
+	}
 
-public void setBoardReplyLev(int boardReplyLev) {
-	this.boardReplyLev = boardReplyLev;
-}
+	public void setBoardReplySeq(int boardReplySeq) {
+		this.boardReplySeq = boardReplySeq;
+	}
 
-public int getBoardReplySeq() {
-	return boardReplySeq;
-}
+	public int getBoardReadCount() {
+		return boardReadCount;
+	}
 
-public void setBoardReplySeq(int boardReplySeq) {
-	this.boardReplySeq = boardReplySeq;
-}
+	public void setBoardReadCount(int boardReadCount) {
+		this.boardReadCount = boardReadCount;
+	}
 
-public int getBoardReadCount() {
-	return boardReadCount;
-}
+	public Date getBoardDate() {
+		return boardDate;
+	}
 
-public void setBoardReadCount(int boardReadCount) {
-	this.boardReadCount = boardReadCount;
-}
+	public void setBoardDate(Date boardDate) {
+		this.boardDate = boardDate;
+	}
 
-public Date getBoardDate() {
-	return boardDate;
-}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 
-public void setBoardDate(Date boardDate) {
-	this.boardDate = boardDate;
-}
-
-public static long getSerialversionuid() {
-	return serialVersionUID;
-}
-
-@Override
-public String toString() {
-	return this.boardNum + ", " + this.boardTitle + ", "
-		+ this.boardWriter + ", " + this.boardContent
-		+ ", " + this.boardReadCount + ", "
-		+ this.boardDate + ", "
-		+ this.boardOriginalFileName + ", "
-		+ this.boardRenameFileName + ", "
-		+ this.boardReplyRef + ", "
-		+ this.boardReplyLev + ", "
-		+ this.boardReplySeq;
-}
-	
-	
+	@Override
+	public String toString() {
+		return this.boardNum + ", " + this.boardTitle + ", "
+			+ this.boardWriter + ", " + this.boardContent
+			+ ", " + this.boardReadCount + ", "
+			+ this.boardDate + ", "
+			+ this.boardOriginalFileName + ", "
+			+ this.boardRenameFileName + ", "
+			+ this.boardRef + ", "
+			+ this.boardReplyRef + ", "
+			+ this.boardReplyLev + ", "
+			+ this.boardReplySeq;
+	}
 }
